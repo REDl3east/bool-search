a5a57dfcf1b2faf6841665707cd319ba0f9e2b23
overmyerD <dalton@raptorpowersystems.com>
2023-02-24 13:27:07 -0600

    added debug flag

--- src/main.cpp
+++ src/main.cpp
@@ -14,11 +14,12 @@ void handle_stdin_println(const size_t line_num, const std::string& line);
 int main(int argc, char** argv) {
   struct arg_lit* recursive_arg = arg_lit0("r", "recursive", "recusivly search given directories");
   struct arg_lit* help_arg      = arg_lit0("h", "help", "display this help and exit");
+  struct arg_lit* debug_arg     = arg_lit0("d", "debug", "outputs a dot file from the given EXPR");
   struct arg_str* expr_arg      = arg_str1(NULL, NULL, "EXPR", "The expression that is used to search");
   struct arg_file* file_arg     = arg_filen(NULL, NULL, "FILE", 0, argc + 2, "The file or directory (if has -r option) to search from");
   struct arg_end* end           = arg_end(20);
 
-  void* argtable[] = {recursive_arg, help_arg, expr_arg, file_arg, end};
+  void* argtable[] = {recursive_arg, help_arg, debug_arg, expr_arg, file_arg, end};
 
   if (arg_nullcheck(argtable) != 0) {
     std::cerr << argv[0] << ": insufficient memory\n";
@@ -33,7 +34,8 @@ int main(int argc, char** argv) {
     arg_print_syntax(stdout, argtable, "\n");
     arg_print_glossary(stdout, argtable, "  %-25s %s\n");
 
-    std::cout << "\n" << "When FILE is absent, read in input from standard input. Read from \".\", if -r option is specified.\n";
+    std::cout << "\n"
+              << "When FILE is absent, read in input from standard input. Read from \".\", if -r option is specified.\n";
 
     arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
     return 0;
@@ -61,6 +63,12 @@ int main(int argc, char** argv) {
 
     arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
     return 1;
+  }
+
+  if (debug_arg->count > 0) {
+    std::cout << p.dot(input);
+    arg_freetable(argtable, sizeof(argtable) / sizeof(argtable[0]));
+    return 0;
   }
 
   if (file_arg->count == 0) {
@@ -143,6 +151,6 @@ void handle_file_println(const std::filesystem::path& path, const size_t line_nu
   std::cout << termcolor::magenta << path.string() << termcolor::blue << ":" << termcolor::green << line_num << termcolor::reset << ": " << termcolor::bold << line << termcolor::reset << '\n';
 }
 
-void handle_stdin_println(const size_t line_num, const std::string& line){
+void handle_stdin_println(const size_t line_num, const std::string& line) {
   std::cout << termcolor::green << line_num << termcolor::reset << ": " << termcolor::bold << line << termcolor::reset << '\n';
 }
